9d7572d7340a008167b5eb200a941826b598f57d
tesol2y090 <gliese090@gmail.com>
2023-06-07 16:59:59 +0700

    add: merge landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,11 @@
 import Head from "next/head"
 
-const navigation = [
-  { name: "Problem", href: "#problem" },
-  { name: "Solution", href: "#solution" },
-  { name: "For Registrants", href: "#project" },
-  { name: "For Auditors", href: "#participate" },
-]
-
-const footerNavigation = {
-  solutions: [
-    { name: "Marketing", href: "#" },
-    { name: "Analytics", href: "#" },
-    { name: "Commerce", href: "#" },
-    { name: "Insights", href: "#" },
-  ],
-  support: [
-    { name: "Pricing", href: "#" },
-    { name: "Documentation", href: "#" },
-    { name: "Guides", href: "#" },
-    { name: "API Status", href: "#" },
-  ],
-  company: [
-    { name: "About", href: "#" },
-    { name: "Blog", href: "#" },
-    { name: "Jobs", href: "#" },
-    { name: "Press", href: "#" },
-    { name: "Partners", href: "#" },
-  ],
-  legal: [
-    { name: "Claim", href: "#" },
-    { name: "Privacy", href: "#" },
-    { name: "Terms", href: "#" },
-  ],
-  social: [
-    {
-      name: "Twitter",
-      href: "https://twitter.com/gangabon",
-      icon: (props: any) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-        </svg>
-      ),
-    },
-    {
-      name: "GitHub",
-      href: "https://github.com/Gangabon-Lab",
-      icon: (props: any) => (
-        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
-          <path
-            fillRule="evenodd"
-            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-
-  ],
-}
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Gangabon Protocol</title>
       </Head>
-      {/* Header */}
-      <header className="absolute inset-x-0 top-0 z-50">
-        <nav
-          className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
-          aria-label="Global"
-        >
-          <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Gangabon</span>
-              <img
-                className="h-8 w-auto"
-                src="/images/logo-with-text.svg"
-                alt=""
-              />
-            </a>
-          </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(true)}
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div className="hidden lg:flex lg:gap-x-12">
-            {navigation.map((item) => (
-              <a
-                key={item.name}
-                href={item.href}
-                className="text-sm font-semibold leading-6 text-gray-900"
-              >
-                {item.name}
-              </a>
-            ))}
-          </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <button className="flex w-40 items-center justify-center rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">
-              Launch App
-            </button>
-          </div>
-        </nav>
-      </header>
-
       <main className="isolate">
         {/* Hero section */}
         <div className="relative isolate -z-10 overflow-hidden bg-gradient-to-b from-indigo-100/20 pt-14">
@@ -124,7 +20,8 @@ export default function Home() {
               </h1>
               <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
                 <p className="text-lg leading-8 text-gray-600">
-                Cheapest, simplest, and fastest way to get certificates that we can trust.
+                  Cheapest, simplest, and fastest way to get certificates that
+                  we can trust.
                 </p>
               </div>
               <img
@@ -146,62 +43,75 @@ export default function Home() {
                   Problem
                 </h2>
                 <p className="mt-6 text-xl leading-8 text-gray-600">
-                Most of renewable energy projects can’t access to certificate or credit issuance because measurement, reporting, verification is only for rich people.
+                  Most of renewable energy projects can’t access to certificate
+                  or credit issuance because measurement, reporting,
+                  verification is only for rich people.
                 </p>
               </div>
 
-            <div className="mx-auto mt-16 flex max-w-2xl flex-col gap-8 lg:mx-0 lg:mt-20 lg:max-w-none lg:flex-row lg:items-end">
-            <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-50 p-8 sm:w-3/4 sm:max-w-md sm:flex-row-reverse sm:items-end lg:w-72 lg:max-w-none lg:flex-none lg:flex-col lg:items-start">
-              <p className="flex-none text-3xl font-bold tracking-tight text-gray-900">
-                5
-              </p>
-              <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
-                <p className="text-lg font-semibold tracking-tight text-gray-900">
-                  Lack of auditors
-                </p>
-                <p className="mt-2 text-base leading-7 text-gray-600">
-                  In my country (Thailand), we only have qualifed 5 auditors.
-                </p>
+              <div className="mx-auto mt-16 flex max-w-2xl flex-col gap-8 lg:mx-0 lg:mt-20 lg:max-w-none lg:flex-row lg:items-end">
+                <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-50 p-8 sm:w-3/4 sm:max-w-md sm:flex-row-reverse sm:items-end lg:w-72 lg:max-w-none lg:flex-none lg:flex-col lg:items-start">
+                  <p className="flex-none text-3xl font-bold tracking-tight text-gray-900">
+                    5
+                  </p>
+                  <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+                    <p className="text-lg font-semibold tracking-tight text-gray-900">
+                      Lack of auditors
+                    </p>
+                    <p className="mt-2 text-base leading-7 text-gray-600">
+                      In my country (Thailand), we only have qualifed 5
+                      auditors.
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-900 p-8 sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-sm lg:flex-auto lg:flex-col lg:items-start lg:gap-y-44">
+                  <p className="flex-none text-3xl font-bold tracking-tight text-white">
+                    $2,000
+                  </p>
+                  <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+                    <p className="text-lg font-semibold tracking-tight text-white">
+                      To get certificates is costly
+                    </p>
+                    <p className="mt-2 text-base leading-7 text-gray-400">
+                      You have to pay for registration, auditing, verification,
+                      consultation, transaction costs.
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-primary p-8 sm:w-11/12 sm:max-w-xl sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-none lg:flex-auto lg:flex-col lg:items-start lg:gap-y-28">
+                  <p className="flex-none text-3xl font-bold tracking-tight text-white">
+                    8-20 Weeks
+                  </p>
+                  <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+                    <p className="text-lg font-semibold tracking-tight text-white">
+                      Time consuming
+                    </p>
+                    <p className="mt-2 text-base leading-7 text-indigo-200">
+                      From registration process to document preparation to
+                      auditing to certificate issuance. It takes like forever.
+                    </p>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-900 p-8 sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-sm lg:flex-auto lg:flex-col lg:items-start lg:gap-y-44">
-              <p className="flex-none text-3xl font-bold tracking-tight text-white">
-                $2,000
-              </p>
-              <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
-                <p className="text-lg font-semibold tracking-tight text-white">
-                  To get certificates is costly
-                </p>
-                <p className="mt-2 text-base leading-7 text-gray-400">
-                  You have to pay for registration, auditing, verification, consultation, transaction costs.
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-primary p-8 sm:w-11/12 sm:max-w-xl sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-none lg:flex-auto lg:flex-col lg:items-start lg:gap-y-28">
-              <p className="flex-none text-3xl font-bold tracking-tight text-white">
-                8-20 Weeks
-              </p>
-              <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
-                <p className="text-lg font-semibold tracking-tight text-white">
-                  Time consuming 
-                </p>
-                <p className="mt-2 text-base leading-7 text-indigo-200">
-                  From registration process to document preparation to auditing to certificate issuance. It takes like forever.
-                </p>
-              </div>
-            </div>
-          </div>
 
-              <div className="lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8" id="solution">
+              <div
+                className="lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8"
+                id="solution"
+              >
                 <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
                   Solution
                 </h2>
                 <p className="mt-6 text-xl leading-8 text-gray-600">
-                We use community-based measurement, reporting, and verification (Also known as Participatory MRV) because smart meter is traceable on blockchain and double counting is better monitored by thousand eyes.
+                  We use community-based measurement, reporting, and
+                  verification (Also known as Participatory MRV) because smart
+                  meter is traceable on blockchain and double counting is better
+                  monitored by thousand eyes.
                 </p>
 
                 <p className="mt-6 text-base leading-7 text-gray-600">
-                We particularly focused on emerging markets and developing countries that are struggling to transition to clean energy infrastructure.
+                  We particularly focused on emerging markets and developing
+                  countries that are struggling to transition to clean energy
+                  infrastructure.
                 </p>
               </div>
               <div className="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
@@ -241,7 +151,10 @@ export default function Home() {
         </div>
 
         {/* For Registrants */}
-        <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8" id="project"> 
+        <div
+          className="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8"
+          id="project"
+        >
           <div className="mx-auto max-w-2xl lg:mx-0">
             <p className="mt-6 text-base leading-5 text-gray-400">
               For Regisrants
@@ -250,7 +163,8 @@ export default function Home() {
               Submit Your Project
             </h2>
             <p className="mt-6 text-base leading-7 text-gray-600">
-              For all renewable energy owners, with Gangabon, you can earn RECs with MUCH LOWER cost and SHORTER time than traiditional MRV ways.
+              For all renewable energy owners, with Gangabon, you can earn RECs
+              with MUCH LOWER cost and SHORTER time than traiditional MRV ways.
             </p>
           </div>
           <div>
@@ -258,16 +172,18 @@ export default function Home() {
               className="airtable-embed"
               src="https://airtable.com/embed/shrOQ6euG7ntN1kRe?backgroundColor=green"
               frameBorder="0"
-              onMouseWheel=""
               width="100%"
               height="533"
-              style={{ background: 'transparent', border: '1px solid #ccc' }}
+              style={{ background: "transparent", border: "1px solid #ccc" }}
             ></iframe>
           </div>
         </div>
 
-         {/* For Auditors */}
-         <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8" id="participate">
+        {/* For Auditors */}
+        <div
+          className="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8"
+          id="participate"
+        >
           <div className="mx-auto max-w-2xl lg:mx-0">
             <p className="mt-6 text-base leading-5 text-gray-400">
               For Auditors
@@ -279,41 +195,13 @@ export default function Home() {
               Get paid directly to your wallet with your expertise.
             </p>
             <div className="mt-6 lg:flex lg:flex-1 lg:justify-center">
-            <button className="flex w-40 items-center justify-center rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">
-              Launch App
-            </button>
-          </div>
-          </div>
-        </div>
-
-  
-      </main>
-
-      {/* Footer */}
-      <footer className="mt-32 sm:mt-20" aria-labelledby="footer-heading">
-        <h2 id="footer-heading" className="sr-only">
-          Footer
-        </h2>
-        <div className="mx-auto max-w-7xl px-6 pb-8 lg:px-8">
-          <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24">
-            <div className="flex space-x-6 md:order-2">
-              {footerNavigation.social.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className="text-gray-400 hover:text-gray-500"
-                >
-                  <span className="sr-only">{item.name}</span>
-                  <item.icon className="h-6 w-6" aria-hidden="true" />
-                </a>
-              ))}
+              <button className="flex w-40 items-center justify-center rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">
+                Launch App
+              </button>
             </div>
-            <p className="mt-8 text-xs leading-5 text-gray-500 md:order-1 md:mt-0">
-              &copy; 2023 Gangabon Protocl, Inc. All rights reserved.
-            </p>
           </div>
         </div>
-      </footer>
-    </div>
+      </main>
+    </>
   )
 }
