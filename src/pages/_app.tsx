63ae9ba9d5fa161d47ff359cf000e9e1b357a64d
tesol2y090 <gliese090@gmail.com>
2023-06-07 11:57:46 +0700

    add: wagmi

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,33 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+import "@rainbow-me/rainbowkit/styles.css"
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit"
+import { configureChains, createClient, WagmiConfig } from "wagmi"
+import { sepolia } from "wagmi/chains"
+import { publicProvider } from "wagmi/providers/public"
+
+export const { chains, provider } = configureChains(
+  [sepolia],
+  [publicProvider()]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: "Gangabon",
+  chains,
+})
+
+export const wgmiClient = createClient({
+  autoConnect: true,
+  provider,
+  connectors,
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wgmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
 }
